89b2d4657d7d65193f0d56b00efd97f62978497d
gprateek93 <gprateek93@gmail.com>
2020-06-18 03:30:27 +0530

    Added comments in the crypto helper code and modified few lines

--- crypto.py
+++ crypto.py
@@ -1,23 +1,35 @@
 from hashlib import sha256
 from Crypto.PublicKey import RSA
 
-def generate_hash(data):
-    data = data.encode('utf-8')
-    message = sha256(data)
-    message = message.hexdigest()
+def generate_hash(data = ""):
+    '''Arguments - data: Type = string, default value = ""
+       This function is used to generate a sha256 hash for a given string.'''
+    message = sha256(data.encode()).digest
     return message
 
 def generate_key():
+    '''This function is used to generate a random RSA public and private key pair.'''
     keyPair = RSA.generate(bits=2048)
     return keyPair
 
-def sign_message(message, keyPair):
-    hash = int.from_bytes(sha256(message.encode()).digest(),byteorder = "big")
+def sign_message(message = "", keyPair = None):
+    '''Arguments - message: Type = string, default value = ""
+                   keyPair: Type = an RSA key pair object, default value = None
+       This function is used to sign the given message  using the RSA private key that can be extracted from the key pair object.''''
+    if keyPair == None:
+        return None
+    hash = int.from_bytes(generate_hash(message),byteorder = "big")
     signature = pow(hash, keyPair.d, keyPair.n)
     return signature
 
-def verify_message(message, public_key, signature):
-    hash = int.from_bytes(sha256(message.encode()).digest(), byteorder = "big")
+def verify_signature(message = "", public_key = None, signature= None):
+    '''Arguments - message: Type = string, default value = ""
+                   public_key: Type = an RSA public key object, default value = None
+                   signature: Type = Byte string, default value = None
+       Given a message and a public key this function matches whether the provided signature is valid for the message or not.''''
+    if signature = None or public_key == None:
+        return False
+    hash = int.from_bytes(generate_hash(message), byteorder = "big")
     hashFromSignature = pow(signature, public_key.e, public_key.n)
     return hash == hashFromSignature
 
