48258ca2f95e601fdd85a816558699e5ab179b9f
gprateek93 <gprateek93@gmail.com>
2020-06-29 18:51:40 +0530

    Modified the transaction class

--- Transaction.py
+++ Transaction.py
@@ -2,37 +2,54 @@ import crypto
 
 class Transaction:
     class Input:
-        def __init__(self,prev_hash,index):
+        def __init__(self,prev_hash = None ,index = None):
             self.prev_hash = prev_hash
             self.index = index
         
-        def addSignature(self,signature):
-            self.signature = signature
+        def addSignature(self, script_sig = None):
+            self.script_sig = script_sig
     
     class Output:
-        def __init__(self, public_key, value):
+        def __init__(self, pubkey = None, value = None):
             self.value = value
-            self.public_key = public_key
+            self.pubkey = pubkey
     
-    def __init__(self, tx = None):
-        if tx == None:
-            self.dictionary = {}
-            self.inputs = []
-            self.outputs = []
-        else:
-            self.dictionary = tx.dictionary
-            self.inputs = tx.inputs
-            self.outputs = tx.outputs
-
+    def __init__(self,version,lock_time):
+        self.version = version
+        self.inputs = []
+        self.outputs = []
+        self.lock_time = lock_time
+    
     def add_input(self, prev_hash, index):
         inp = Transaction.Input(prev_hash,index)
         self.inputs.append(inp)
     
-    def add_output(self, public_key, value):
-        out = Transaction.Output(public_key,value)
+    def add_output(self, pubkey, value):
+        out = Transaction.Output(pubkey,value)
         self.outputs.append(out)
 
     def remove_input(self,index):
         inputs.pop(index)
-    
+
+    def getRawTx(self):
+        rawTx = ""
+        rawTx+= str(self.version)
+        for inp in self.inputs:
+            if inp.prev_hash is not None:
+                rawTx += str(inp.prev_hash)
+            if inp.index is not None:
+                rawTx += str(inp.index)
+            if inp.script_sig is not None:
+                rawTx += str(inp.script_sig)
+        for out in self.outputs:
+            if out.value is not None:
+                rawTx += str(out.value)
+            if out.pubkey is not None:
+                rawTx += str(out.pubkey)
+        rawTx += self.lock_time
+        rawTx = bytes(rawTx , "utf-8")
+        return rawTx
+
+    def finishTx(self):
+        self.hash  = crypto.generate_hash(self.getRawTx())
     
