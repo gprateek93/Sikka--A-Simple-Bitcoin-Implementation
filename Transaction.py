6826a651ee5d5142948c330e58619926c89cd248
gprateek93 <gprateek93@gmail.com>
2020-07-02 01:22:42 +0530

    Added some functions in the Transaction.py

--- Transaction.py
+++ Transaction.py
@@ -6,14 +6,31 @@ class Transaction:
             self.prev_hash = prev_hash
             self.index = index
         
-        def addSignature(self, script_sig = None):
-            self.script_sig = script_sig
-    
+        def addSignature(self, signature = None):
+            self.signature = signature
+        
+        def addUnlockingScript(self, signature = None, pubkey = None):
+            assert signature is not None, "A certain unlocking script can't have signature as None"
+            assert pubkey is not None, "A certain unlocking script can't have public key as None"
+            self.unlocking_script = str(signature) + " " + str(pubkey)
+        
+        def __eq__(self,other):
+            if self.prev_hash == other.prev_hash and self.index == other.index and self.unlocking_script == other.unlocking_script:
+                return True
+            return False
+
     class Output:
         def __init__(self, pubkey = None, value = None):
             self.value = value
             self.pubkey = pubkey
-    
+            pubkey_hash = crypto.generate_hash(pubkey)
+            self.locking_script = "OP_DUP" + " " + "OP_HASH160" + " " + str(pubkey_hash) + " " + "OP_EQUALVERIFY" + " " + "OP_CHECKSIG"
+
+        def __eq__(self,other):
+            if self.value == other.value and self.locking_script == other.locking_script:
+                return True
+            return False
+
     def __init__(self,version = 0,lock_time = 0, coin = -1, address = None):
         if(coin == -1):
             self.coinbase = False
@@ -83,8 +100,28 @@ class Transaction:
         rawTx = bytes(rawTx , "utf-8")
         return rawTx
 
-    def addSignature(self, index = -1, script_sig = None):
+    def addSignature(self, index = -1, signature = None):
         assert index < len(self.inputs) and index >= 0 , "Index out of bounds"
-        assert script_sig is not None, "Unlocking script of a certain input can't be None"
-        self.inputs[index].addSignature(script_sig)
+        assert signature is not None, "Signature of a certain input can't be None"
+        self.inputs[index].addSignature(signature)
+
+    def total_outputs(self):
+        return len(self.outputs)
+
+    def total_inputs(self):
+        return len(self.inputs)
+
+    def getInput(self,index):
+        return self.inputs[index]
+
+    def getOutput(self,index):
+        return self.outputs[index]
     
+    def getHash(self):
+        return self.hash
+
+    def __eq__(self, other):
+        if self.hash!=other.hash or self.coinbase != other.coinbase or self.version != other.version or self.inputs != other.inputs or self.outputs != self.outputs or self.lock_time != other.lock_time:
+            return False
+        else:
+            return True
