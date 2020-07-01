aacea61f8654d4ce6276d8cf96d3f2f9528a354b
PhaneeshB <phaneesh@LAPTOP-A1E8DJDT.localdomain>
2020-07-02 00:14:12 +0530

    Add Unspent Transactions Pool

---
+++ UTXO_pool.py
@@ -0,0 +1,20 @@
+
+class UTXO_pool():
+	def __init__(self):
+		self.mappings = dict({})
+
+	def add_UTXO(self, utxo, txn_output):
+		# add the utxo and txn output
+		self.mappings[utxo.hash_index_tup] = txn_output
+
+	def delete_utxo(self, utxo):
+		del self.mappings[utxo.hash_index_tup]
+	
+	def get_txn_output(self, utxo):
+		return self.mappings[utxo.hash_index_tup]
+	
+	def has_utxo(self, utxo):
+		return utxo.hash_index_tup in self.mappings
+
+	def get_all_utxo(self):
+		return list(self.mappings.keys())
