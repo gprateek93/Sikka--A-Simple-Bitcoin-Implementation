5fe30a12315e5795a8b140591ca5e3f43dc972d1
gprateek93 <gprateek93@gmail.com>
2020-07-07 03:27:07 +0530

    Added genesis block creation

--- Block.py
+++ Block.py
@@ -1,19 +1,41 @@
-import Transaction
-import merkle
-from hashlib import sha256
-from constants import merkle_arity
+from Transaction import Transaction
+from merkle import MerkleTree
+import crypto
+from constants import miner_reward
+import Miner
+import logging
+
+
 class Block:
 
-	def __init__(self, prev_hash, address):
-		self.coinbase_value = 25
-		self.__hash = None
-		self.__prev_block_hash = prev_hash
-		self.__coinbase_txn = Transaction(coinbase_value, address)
-		self.__txns = []
-		self.__merkle_tree_root = None
-		self.__merkle_arity = merkle_arity
-		self.__nonce = 0
-		self.__address = address # public key of miner
+	def __init__(self, prev_hash= None, node_pool= None, address=None, genesis_block = False):
+		logging.basicConfig(filename=f'LOG-Block.log', level=logging.DEBUG, format='%(asctime)s : %(levelname)s :Process- %(process)d %(processName)s: Thread- %(thread)d %(threadName)s: %(funcName)s : %(message)s')
+		if genesis_block:
+			self.__coinbase_txn = self.create_genesis_block(node_pool)
+			self.__txns = [self.__coinbase_txn]
+			self.__hash = None
+			self.__merkle_tree_root = None
+			self.__nonce = 0
+			self.__prev_block_hash = None
+			logging.info(f"Genesis block created")
+		else:
+			self.__hash = None
+			self.__prev_block_hash = prev_hash
+			self.__coinbase_txn = Transaction(miner_reward, address)
+			self.__coinbase_txn.finalize()
+			self.__txns = [self.__coinbase_txn]
+			self.__merkle_tree_root = None
+			self.__nonce = 0
+			self.__address = address # public key of miner
+
+	def create_genesis_block(self, node_pool=None):
+		assert len(node_pool) > 0, "The system cannot have 0 nodes initially during the creation of genesis block"
+		genesis_txn = Transaction(version= 0, lock_time= 0, coin= 100, address = node_pool[0].get_address())
+		for nodes in node_pool[1:]:
+			pubkey = node.get_address()
+			genesis_txn.add_output(pubkey,100)
+		genesis_txn.finalize()
+		return genesis_txn
 
 	
 	def get_coinbase_txn(self):
@@ -39,24 +61,20 @@ class Block:
 		self.__nonce += 1
 
 	def construct_merkle_tree(self):
-		mt = MerkleTree(self.__merkle_arity, self.__txns)
+		mt = MerkleTree(self.__txns)
 		self.__merkle_tree_root = mt.root
+		
 
 	def get_raw_block(self):
-		raw_block = str(self.__nonce) + self.__merkle_tree_root
+		raw_block = str(self.__nonce) + self.__merkle_tree_root.hash
 
 		if self.__prev_block_hash != None:
 			# hex string
 			raw_block += self.__prev_block_hash[2:]
-		for i in range(self.__txns):
+		for i in range(len(self.__txns)):
 			# str of tnx
-			raw_block += self.__txns[i].getRawTx()
+			raw_block += self.__txns[i].get_raw_txn()
 		return raw_block
 
-	def generate_hash(self):
-		m = sha256()
-		m.update(bytes(self.get_raw_block(), encoding='UTF-8'))
-		self.__hash = m.hexdigest()
-
-
-
+	def finalize(self):
+		self.__hash = hex(crypto.generate_hash(self.get_raw_block()))
