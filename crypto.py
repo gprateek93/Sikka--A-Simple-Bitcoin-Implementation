80a8f71b9129dffcfdcc170174dbcbd76ede275f
gprateek93 <gprateek93@gmail.com>
2020-07-07 03:24:18 +0530

    Add minor corrections in the crypto helper file

--- crypto.py
+++ crypto.py
@@ -4,6 +4,7 @@ from Crypto.PublicKey import RSA
 def generate_hash(data = ""):
     '''Arguments - data: Type = string, default value = ""
        This function is used to generate a sha256 hash for a given string.'''
+    assert type(data) == str, "crypto.generate_hash argument data is not string"
     message = sha256(data.encode()).digest()
     return int.from_bytes(message, byteorder = "big")
 
@@ -16,20 +17,20 @@ def sign_message(message = "", keyPair = None):
     '''Arguments - message: Type = string, default value = ""
                    keyPair: Type = an RSA key pair object, default value = None
        This function is used to sign the given message  using the RSA private key that can be extracted from the key pair object.'''
-    if keyPair == None:
-        return None
+    # if keyPair == None:
+    #     return None
     hash = generate_hash(message)
     signature = pow(hash, keyPair.d, keyPair.n)
-    return signature
+    return hex(signature)
 
 def verify_signature(message = "", public_key = None, signature= None):
     '''Arguments - message: Type = string, default value = ""
                    public_key: Type = an RSA public key object, default value = None
                    signature: Type = Byte string, default value = None
        Given a message and a public key this function matches whether the provided signature is valid for the message or not.'''
-    if signature == None or public_key == None:
-        return False
+    # if signature == None or public_key == None:
+    #     return False
     hash = generate_hash(message)
-    hashFromSignature = pow(signature, public_key.e, public_key.n)
+    hashFromSignature = pow(int(signature,16), public_key.e, public_key.n)
     return hash == hashFromSignature
 
