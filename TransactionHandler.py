37e453783c288018e130130cd38f134b86d6ef4f
gprateek93 <gprateek93@gmail.com>
2020-07-07 03:30:01 +0530

    Add minor changes in the Transaction Handler

--- TransactionHandler.py
+++ TransactionHandler.py
@@ -2,6 +2,10 @@ import crypto
 import UTXO
 import opcodes
 import UTXO_pool
+import script
+from Transaction import Transaction
+from UTXO_pool import UTXO_pool
+from UTXO import UTXO
 
 class TransactionHandler:
     def __init__(self, utxopool):
@@ -11,22 +15,22 @@ class TransactionHandler:
         unique_utxo = UTXO_pool()
         input_sum = 0
         output_sum = 0
-        for i in tx.total_inputs():
-            inp = tx.getInput(i)
+        for i in range(tx.total_inputs()):
+            inp = tx.get_input(i)
             utxo = UTXO(inp.prev_hash, inp.index)
             if not self.utxopool.has_utxo(utxo):
                 return False
-            out = self.utxopool[utxo]
-            verification_script = inp.unlocking_script + " " out.locking_script + " " + "OP_VERIFY"
-            if not opcodes.execute_script(verification_script,[],tx):
+            out = self.utxopool.mappings[utxo]
+            verification_script = inp.unlocking_script + out.locking_script + ["OP_VERIFY"]
+            if not script.execute_script(verification_script,tx,i):
                 return False
             if unique_utxo.has_utxo(utxo):
                 return False
             unique_utxo.add_UTXO(utxo, out)
             input_sum += out.value
 
-        for i in tx.total_outputs():
-            out = tx.getOutput(i)
+        for i in range(tx.total_outputs()):
+            out = tx.get_output(i)
             if out.value<0:
                 return False
             output_sum += out.value
@@ -41,11 +45,10 @@ class TransactionHandler:
                 for inp in tx.inputs:
                     utxo = UTXO(inp.prev_hash, inp.index)
                     self.utxopool.delete_utxo(utxo)
-                for i in tx.total_outputs():
-                    utxo = UTXO(tx.getHash(), i)
-                    self.utxopool.add_UTXO(utxo)
+                for i in range(tx.total_outputs()):
+                    utxo = UTXO(tx.get_hash(), i)
+                    self.utxopool.add_UTXO(utxo,tx.get_output(i))
         
         validTX = list(set(validTX))
         return validTX
-            
-
+        
