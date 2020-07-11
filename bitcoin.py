86ac25e098211ab91140146dcf7b23c8792a2b2f
gprateek93 <gprateek93@gmail.com>
2020-07-11 20:49:07 +0530

    Add bitcoin simulation code

--- bitcoin.py
+++ bitcoin.py
@@ -1,21 +1,48 @@
-import Miner
-from concurrent.futures import ThreadPoolExecutor
-
-def run_node(node):
-	print(f'Running {os.getpid()} with node id {node.id}') 
-	node.spawn()
+from Miner import Miner
+from Block import Block
+from Blockchain import Blockchain
+from multiprocessing.managers import BaseManager
+from concurrent.futures import ProcessPoolExecutor
+from MessageClass import Queue_Event_Lock
+from constants import num_seed_nodes,num_non_seed_nodes
+from Miner_utils import calc_proof_of_work
 
 if __name__ == "__main__":
+
+	class MyManager(BaseManager):
+		pass  
+
+	MyManager.register('QEL', Queue_Event_Lock)
 	main_node_pool = []
 	seed_nodes = dict({})
-	for i in range(2):
-		new_seed = Miner(miner_id=i,node_type="SEED")
-		seed_nodes[i] = (new_seed.lock, new_seed.message_queue)
+	manager = MyManager()
+	manager.start()
+
+	# create genesis block
+	for i in range(num_seed_nodes):    
+		# seed_nodes[i] = new_seed.get_QEL()
+		seed_nodes[i] = manager.QEL()
+		new_seed = Miner(miner_id=i, blockchain = None, manager_qel=seed_nodes[i], node_type="SEED")
 		main_node_pool.append(new_seed)
 
-	test_node = Miner(miner_id=3, seed_node_pool=seed_nodes, node_type="NON-SEED")
-	main_node_pool.append(test_node)
-	print("here")
-	with ThreadPoolExecutor(max_workers=3) as executor:
-		print("here")
-		executor.map(run_node,main_node_pool)
+	# create genesis blockchain with cionbase for each seed node
+	block = Block(node_pool= main_node_pool, genesis_block=True)
+	block.construct_merkle_tree()
+	print('Node 0 ki compute power', main_node_pool[0].compute_power)
+	# block = calc_proof_of_work(block, main_node_pool[0].compute_power)
+	block = calc_proof_of_work(block, 2)
+	print('Done computing')
+
+	gen_blockchain = Blockchain(genesis_block=block)
+	for i in range(num_seed_nodes):
+		main_node_pool[i].blockchain = gen_blockchain
+
+	for i in range(num_seed_nodes, num_non_seed_nodes + num_seed_nodes):
+		test_node = Miner(miner_id=i, blockchain = None, manager_qel=manager.QEL(), seed_node_pool=seed_nodes, node_type="NON-SEED")
+		main_node_pool.append(test_node)
+
+	with ProcessPoolExecutor(max_workers=num_non_seed_nodes+num_seed_nodes+2) as executor:
+		for node in main_node_pool:
+			executor.submit(node.spawn)
+
+	print('Done with everything')
