e687f09d7d975089c90f529b7062270d67ba9526
PhaneeshB <b.phaneesh@gmail.com>
2020-07-11 18:11:44 +0530

    Update Min neighbours

--- constants.py
+++ constants.py
@@ -5,5 +5,5 @@ merkle_arity = 2
 num_block_txns = 10
 num_non_seed_nodes = 2
 num_seed_nodes = 2
-max_neighbours = num_seed_nodes + num_non_seed_nodes//2
+min_neighbours = num_seed_nodes + num_non_seed_nodes//2
 genesis_prev_block_hash = hex(0)
